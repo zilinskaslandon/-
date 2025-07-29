238b8bc1041c77c483452872c5b190d24973d81a
zilinskaslandon <zilinskaslandon@gmail.com>
2025-07-30 00:16:56 +0800

    Add files via upload

--- BJX脚本中心.lua
+++ BJX脚本中心.lua
@@ -76,9 +76,37 @@ MainTab:AddButton({
 })
 
 MainTab:AddButton({
+    Title = "踏空行走",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostPlayer352/Test4/main/Float"))()
+    end
+})
+
+MainTab:AddButton({
+    Title = "最强透视",
+    Callback = function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/uw2P2fbY"))()
+    end
+})
+
+MainTab:AddButton({
     Title = "玩家加入提示",
     Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/boyscp/scriscriptsc/main/bbn.lua"))()
+    end
+})
+
+MainTab:AddButton({
+    Title = "死亡笔记",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/dingding123hhh/tt/main/%E6%AD%BB%E4%BA%A1%E7%AC%94%E8%AE%B0%20(1).txt"))()
+    end
+})
+
+MainTab:AddButton({
+    Title = "电脑键盘",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt"))()
     end
 })
 
@@ -131,5 +159,186 @@ MainTab:AddToggle({
                 Stepped = nil
             end
         end
+    end
+})
+
+local MainTab = Window:MakeTab({"汽车经销大亨", "cool"})
+
+
+MainTab:AddButton({
+    Title = "刷星星",
+    Callback = function()
+        loadstring(game:HttpGet("https://scriptblox.com/raw/LIMITED!-Car-Dealership-Tycoon-Moon-Team-16181"))()
+    end
+})
+
+MainTab:AddButton({
+    Title = "1/脚本",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/IExpIoit/Script/main/Car%20Dealership%20Tycoon.lua"))()
+    end
+})
+
+MainTab:AddButton({
+    Title = "2/脚本",
+    Callback = function()
+        loadstring(game:HttpGet("https://pastefy.app/5o594Q0i/raw"))()
+    end
+})
+
+MainTab:AddButton({
+    Title = "3/脚本",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/03sAlt/BlueLockSeason2/main/README.md"))()
+    end
+})
+
+local MainTab = Window:MakeTab({"忍者传奇", "cool"})
+
+
+MainTab:AddButton({
+    Title = '出生',  -- 使用 Title 作为显示名称
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(25.665, 2.423, 29.920)
+    end
+})
+
+MainTab:AddButton({
+    Title = '魔法',
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(51.172, 766.181, -138.449)
+    end
+})
+
+MainTab:AddButton({
+    Title = '置岛',
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(207.299, 2013.880, 237.367)
+    end
+})
+
+MainTab:AddButton({
+    Title = '密岛',
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(171.972, 4091.381, 42.070)
+    end
+})
+
+MainTab:AddButton({
+    Title = '天空岛',
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(148.838, 5657.185, 73.591)
+    end
+})
+
+MainTab:AddButton({
+    Title = '冷岛',
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(139.283, 9285.184, 71.364)
+    end
+})
+
+MainTab:AddButton({
+    Title = '无限岛',  
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(149.348, 13680.031, 73.386)
+    end
+})
+
+MainTab:AddButton({
+    Title = '沙岛',
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(133.372, 17686.328, 72.004)
+    end
+})
+
+MainTab:AddButton({
+    Title = '雷岛',
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(143.193, 24970.021, 78.059)
+    end
+})
+
+MainTab:AddButton({
+    Title = '达古塔教岛',
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(141.272, 28256.295, 69.379)
+    end
+})
+
+MainTab:AddButton({
+    Title = '暗影岛',
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(132.743, 33206.989, 57.496)
+    end
+})
+
+MainTab:AddButton({
+    Title = '灵魂岛',
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(137.761, 29917.571, 61.066)
+    end
+})
+
+MainTab:AddButton({
+    Title = '黄金岛', 
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(128.322, 52607.766, 56.694)
+    end
+})
+
+MainTab:AddButton({
+    Title = '传奇岛',
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(146.352, 59594.680, 71.532)
+    end
+})
+
+MainTab:AddButton({
+    Title = '科技传奇岛',
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(131.232, 66669.164, 72.217)
+    end
+})
+
+MainTab:AddButton({
+    Title = '超能岛',
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(135.987, 70211.156, 57.023)
+    end
+})
+
+MainTab:AddButton({
+    Title = '混沌传奇岛',
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(148.584, 74492.852, 69.317)
+    end
+})
+
+MainTab:AddButton({
+    Title = '黑暗岛',
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(136.970, 79746.984, 58.541)
+    end
+})
+
+MainTab:AddButton({
+    Title = "黑暗元素岛",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(141.697, 83198.984, 72.731)
+    end
+})
+
+MainTab:AddButton({
+    Title = "和平岛",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(135.316, 87051.063, 66.784)
+    end
+})
+
+MainTab:AddButton({
+    Title = "旋涡岛",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(135.082, 91246.070, 69.567)
     end
 })
