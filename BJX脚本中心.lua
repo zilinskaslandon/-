986d80544330d1bcc57d570de5ef74910f1cbaa2
zilinskaslandon <zilinskaslandon@gmail.com>
2025-07-29 21:39:46 +0800

    Add files via upload

---
+++ BJX脚本中心.lua
@@ -0,0 +1,100 @@
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/gycgchgyfytdttr/QQ-9-2-8-9-50173/refs/heads/main/Ul%E6%BA%90%E7%A0%81.lua"))()
+
+local Window = redzlib:MakeWindow({
+  Title = "北极星脚本中心",
+  SubTitle = "BJX",
+  SaveFolder = "Redz Config"
+})
+
+print("反挂机已开启")
+Start = tick()
+		local vu = game:GetService("VirtualUser")
+		game:GetService("Players").LocalPlayer.Idled:connect(function()
+		   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+		   wait(1)
+		   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+		end)
+
+Window:AddMinimizeButton({
+  Button = { Image = "rbxassetid://98541457845136", BackgroundTransparency = 0, Size = UDim2.fromOffset(60, 60), },
+  Corner = { CornerRadius = UDim.new(0, 10) }
+})
+
+local MainTab = Window:MakeTab({"主功能", "cool"})
+
+MainTab:AddButton({
+    Title = "飞行",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/zilinskaslandon/-/refs/heads/main/%E5%8C%97%E6%9E%81%E6%98%9F%E9%A3%9E%E8%A1%8C.Lua"))()
+    end
+})
+
+MainTab:AddButton({
+    Title = "阿尔宙斯飞行",
+    Callback = function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/5zJu3hfN"))()
+    end
+})
+
+MainTab:AddToggle({
+    Title = "速度 (开/关)",
+    Default = false,
+    Callback = function(v)
+        if v == true then
+            sudu = game:GetService("RunService").Heartbeat:Connect(function()
+                if game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character.Humanoid and game:GetService("Players").LocalPlayer.Character.Humanoid.Parent then
+                    if game:GetService("Players").LocalPlayer.Character.Humanoid.MoveDirection.Magnitude > 0 then
+                        game:GetService("Players").LocalPlayer.Character:TranslateBy(game:GetService("Players").LocalPlayer.Character.Humanoid.MoveDirection * Speed / 10)
+                    end
+                end
+            end)
+        elseif not v and sudu then
+            sudu:Disconnect()
+            sudu = nil
+        end
+    end
+})
+
+MainTab:AddSlider({
+    Title = "速度设置",
+    Description = "拉条",
+    Default = 1,
+    Min = 1,
+    Max = 1000,
+    Callback = function(v)
+        Speed = v
+    end
+})
+
+MainTab:AddButton({
+    Title = "无限跳跃",
+    Callback = function()
+        game:GetService("UserInputService").JumpRequest:connect(function()
+            game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")		
+        end)
+    end
+})
+
+MainTab:AddButton({
+    Title = "玩家加入提示",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/boyscp/scriscriptsc/main/bbn.lua"))()
+    end
+})
+
+MainTab:AddButton({
+    Title = "反挂机V1",
+    Callback = function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/9fFu43FF"))()
+    end
+})
+
+MainTab:AddButton({
+    Title = "夜视",
+    Callback = function()
+        loadstring(game:HttpGet("        if state then
+        game.Lighting.Ambient = Color3.new(1, 1, 1)
+        else
+            game.Lighting.Ambient = Color3.new(0, 0, 0)"))()
+    end
+})
